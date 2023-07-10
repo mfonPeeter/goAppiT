7cdcc33a82d56122657afaf80754a951e890d789
mfonPeeter <godimfon@gmail.com>
2023-07-10 06:30:47 +0100

    changed the text of the hero section and added a lighter bg-gradient

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -8,14 +8,17 @@ const Hero = () => {
       <div className="container mx-auto max-w-[1450px] flex flex-col-reverse space-y-reverse space-y-10 items-center justify-between lg:flex-row lg:space-y-0 lg:space-x-2 xl:gap-24">
         <div className="w-full lg:w-1/2">
           <h1 className="mb-4 max-w-3xl mx-auto text-center text-3xl font-bold lg:max-w-2xl lg:mx-0 lg:text-left lg:text-4xl xl:text-5xl">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#fb2965] to-[#fa1154]">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#fb4176] to-[#fa1154]">
               GoAPPiT
             </span>{" "}
-            - The Top Software Development Company to Empower you with AI
+            - Software Development to Empower your Business
           </h1>
           <p className="mb-8 max-w-xl mx-auto text-center text-lg lg:text-xl lg:mx-0 lg:text-left">
-            Get ready for a development environment that can finally catch up
-            with you. It doesn't matter where you are at.
+            Stay ahead of the technology curve with our in-depth coverage of
+            sofware development and system integration. Explore the latest
+            insights and solutions for seamless integration and efficient
+            software creation, ensuring you remain at the forefront of
+            innovation.
           </p>
           <div className="flex justify-center lg:flex lg:justify-start">
             <Link
